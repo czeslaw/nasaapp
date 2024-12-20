e3133add404954a649433f7c66d65302f6ad47c3
Piotr Nietrzebka <piotr.nietrzebka@gmail.com>
2024-12-20 13:40:48 +0100

    Initial project commit.

---
+++ NASAapp/NASAappUITests/NASAappUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NASAappUITestsLaunchTests.swift
+//  NASAappUITests
+//
+//  Created by Piotr Nietrzebka on 20/12/2024.
+//
+
+import XCTest
+
+final class NASAappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
